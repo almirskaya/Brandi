2874ea365bd045137daa4e75b64c58c8e1a4f318
almirskaya <alexamirskaya@gmail.com>
2016-11-15 12:14:19 +0200

    project has been created

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+var watch = require('gulp-watch');
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+
+
+gulp.task("default", function () {
+  return gulp.src("src/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("all.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist"));
+});
